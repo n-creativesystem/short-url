f15ccf9e892c699b6ab59455ec71ed2079059238
nonchan7720 <nozomi_nishinohara@n-creativesystem.com>
2023-07-01 05:29:21 +0000

    fix Table

---
+++ frontend/src/components/Parts/Table/Data/DataTable.tsx
@@ -0,0 +1,48 @@
+export type {
+  GridColDef,
+  GridRenderCellParams,
+  GridValueGetterParams,
+} from '@mui/x-data-grid';
+import { Skeleton } from '@/components/Parts/Loading';
+import { LoadingContext } from '@/components/hooks/Context';
+import {
+  DataGrid,
+  DataGridProps,
+  GridPaginationInitialState,
+  jaJP,
+} from '@mui/x-data-grid';
+import classNames from 'classnames';
+import { FC, useContext } from 'react';
+import styles from './index.module.scss';
+
+const cx = classNames.bind(styles);
+
+export type Props = {
+  className?: string;
+} & DataGridProps;
+
+export const DataTable: FC<Props> = ({ className, ...props }) => {
+  const pagination: GridPaginationInitialState = {
+    paginationModel: {
+      page: 0,
+      pageSize: 50,
+      ...(props.initialState?.pagination?.paginationModel || {}),
+    },
+  };
+  const loading = useContext(LoadingContext);
+
+  return loading ? (
+    <Skeleton width={styles.containerWidth} height={styles.containerHeight} />
+  ) : (
+    <div className={cx(styles.container, className)}>
+      <DataGrid
+        {...props}
+        initialState={{
+          pagination: pagination,
+          ...props.initialState,
+        }}
+        localeText={jaJP.components.MuiDataGrid.defaultProps.localeText}
+      />
+    </div>
+  );
+};

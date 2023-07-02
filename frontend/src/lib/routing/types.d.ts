4367560bfd589a26d057d2f5fd5a8b85cd77b151
nonchan7720 <nozomi_nishinohara@n-creativesystem.com>
2023-07-02 05:54:26 +0000

    fix routing typing

--- frontend/src/lib/routing/types.d.ts
+++ frontend/src/lib/routing/types.d.ts
@@ -1,10 +1,6 @@
-import type { ReadonlyDeep } from 'type-fest';
-
 export type RouteProps = {
   path: string;
   auth?: boolean;
   Component?: LazyExoticComponent<FC>;
   routes?: RouteProps[];
 } & Omit<BaseRouteProps, 'children' | 'element'>;
-
-export type RoutesDef = ReadonlyArray<ReadonlyDeep<RouteProps>>;

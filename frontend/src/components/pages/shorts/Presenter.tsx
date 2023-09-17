0a165b6b163d54c7e22fb8d5da441109fb8e3a03
nonchan7720 <nozomi_nishinohara@n-creativesystem.com>
2023-09-17 11:16:07 +0000

    fix

--- frontend/src/components/pages/shorts/Presenter.tsx
+++ frontend/src/components/pages/shorts/Presenter.tsx
@@ -1,4 +1,3 @@
-import { RegisterButton } from '@/components/Parts/Table';
 import { FC, memo } from 'react';
 import { Table } from './Table';
 import { Url } from './graphql';
@@ -8,14 +7,12 @@ type Props = {
   deleteHandler: (key: string) => () => Promise<void>;
 };
 
-export const Presenter: FC<Props> = memo(
-  ({ data, registerHandler, deleteHandler }) => {
-    return (
-      <>
-        <RegisterButton handler={registerHandler} />
-        <Table data={data} deleteHandler={deleteHandler} />
-      </>
-    );
-  }
-);
+export const Presenter: FC<Props> = memo(({ data, deleteHandler }) => {
+  return (
+    <>
+      {/* <RegisterButton handler={registerHandler} /> */}
+      <Table data={data} deleteHandler={deleteHandler} />
+    </>
+  );
+});
 Presenter.displayName = 'Presenter';
